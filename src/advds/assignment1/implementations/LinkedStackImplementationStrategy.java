e21497d6d6f4abfabf5d140e469680fda9ca0df6
Gokul <gokulnair5@gmail.com>
2022-02-25 20:15:09 +0000

    added stack implementation classes

---
+++ src/advds/assignment1/implementations/LinkedStackImplementationStrategy.java
@@ -0,0 +1,44 @@
+package advds.assignment1.implementations;
+
+import advds.assignment1.dto.DailyCasesDTO;
+import advds.assignment1.util.reader.DataSetReader;
+
+public class LinkedStackImplementationStrategy implements ImplementationStrategy<DailyCasesDTO>{
+
+	@Override
+	public void loadData(DataSetReader<DailyCasesDTO> reader) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DailyCasesDTO search(String key1, String key2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DailyCasesDTO getLatest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DailyCasesDTO getFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sort() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
